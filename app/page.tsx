30c656c79e9146ac25ed64c58f3f280923f2dc7d
Nivesh Sharma <niveshsharma67@gmail.com>
2025-10-28 18:38:43 +0530

    inngest workflows setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
-import { requireAuth } from '@/lib/auth-utils'
-import { caller } from '@/trpc/server'
+'use client'
+import { Button } from '@/components/ui/button'
+import { useTRPC } from '@/trpc/client'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
 
-export default async function Home() {
-  await requireAuth()
-  const data = await caller.getUsers()
+export default function Home() {
+  const trpc = useTRPC()
+  const queryClient = useQueryClient()
+  const { data } = useQuery(trpc.getWorkflows.queryOptions())
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success("job queued")
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions())
+      },
+    }),
+  )
   return (
-    <div className="">
+    <div className="min-h-screen flex justify-center items-center min-w-screen flex-col gap-6">
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Click
+      </Button>
     </div>
   )
 }
--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,12 +1,19 @@
 import prisma from '@/lib/db'
 import { createTRPCRouter, protectedProcedure } from '../init'
+import { inngest } from '@/inngest/client'
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findUnique({
-      where: {
-        id: ctx.auth.user.id,
+  getWorkflows: protectedProcedure.query(() => {
+    return prisma.workflow.findMany()
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: 'test/hello.world',
+      data: {
+        email: 'nivesh@gmail.com',
       },
     })
+
+    return { success: true, message: 'Job queued' }
   }),
 })
 // export type definition of API

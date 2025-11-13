b56074e28811776f3243f859432a1a8ee6f0f8f4
Nivesh Sharma <niveshsharma67@gmail.com>
2025-11-13 10:58:11 +0530

    new config for prisma error

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,10 @@ const nextConfig: NextConfig = {
       },
     ]
   },
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+    '/*': ['./node_modules/.prisma/client/**/*'],
+  },
 }
 
 export default nextConfig
